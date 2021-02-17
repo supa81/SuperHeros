efb121a0fdd2fe6e1c11dd130f3d140499f4d500
stephon nelson <stephon.nelson81@gmail.com>
2021-02-17 14:29:21 -0600

    Added superherp home and create page with test hero

--- SuperHero/Controllers/SuperHeroController.cs
+++ SuperHero/Controllers/SuperHeroController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SuperHero.Data;
+using SuperHero.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,9 @@ namespace SuperHero.Controllers
         // GET: SuperHeroController
         public ActionResult Index()
         {
-            return View();
+            var AllSuperHeros = _context.Superhero;
+
+            return View(AllSuperHeros);
         }
 
         // GET: SuperHeroController/Details/5
@@ -32,16 +35,19 @@ namespace SuperHero.Controllers
         // GET: SuperHeroController/Create
         public ActionResult Create()
         {
+           
             return View();
         }
 
         // POST: SuperHeroController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Superhero hero)
         {
             try
             {
+                _context.Superhero.Add(hero);
+                _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
--- SuperHero/Models/SuperHero.cs
+++ SuperHero/Models/SuperHero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,10 +12,15 @@ namespace SuperHero.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("Super Hero Name")] 
         public string SuperHeroName { get; set; }
+        [DisplayName("Super Hero Alter Ego ")]
         public string SuperHeroAlterEgo { get; set; }
+        [DisplayName("Primary Abilities")]
         public string Primarysuperheroability { get; set; }
+        [DisplayName("Secondary Abilities")]
         public string Secondarysuperheroability { get; set; }
+        [DisplayName("Catch Phrase")]
         public string CatchPhrase { get; set; }
     }
 

6bc009dad1a1b26aa0856707346a391efa3bd65e
stephon nelson <stephon.nelson81@gmail.com>
2021-02-17 11:37:36 -0600

    initial

---
+++ SuperHero/Models/SuperHero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHero.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CrewSize { get; set; }
+        public DateTime LaunchDate { get; set; }
+    }
+}
